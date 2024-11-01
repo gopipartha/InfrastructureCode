57d356977590a43765906d0946405a1063bcdc9f
gopipartha <gopinathparthasarathy04@gmail.com>
2024-11-01 14:35:02 -0500

    Update main.tf
    terraform3

--- src/main.tf
+++ src/main.tf
@@ -1,10 +1,18 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
   backend "s3" {
     bucket = "orangetexinfraterraformbucket"
     key    = "arn:aws:kms:us-east-1:970547365374:key/949d78bc-b5d2-40bf-8b27-b70068c6a64a"
     region = "us-east-1"
   }
 }
+
 
 provider "aws" {
   region = "us-east-2"
